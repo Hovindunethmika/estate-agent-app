bb65a845c47bfd440bd0fe4dfebf0ace8c1bc768
GitHub User <user@github.com>
2026-01-03 23:30:11 +0530

    Update property grid to 2-column layout on all screen sizes for improved UX

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -77,7 +77,7 @@ const PropertyList = ({ properties, onAddToFavourites, onViewDetails, onDragStar
       </div>
 
       {/* Properties Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {sortedProperties.map(property => (
           <PropertyCard 
             key={property.id} 
